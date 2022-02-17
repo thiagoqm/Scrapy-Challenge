69f48ba20d140617a61b233c4e2a2a48ec328edf
thiagoqm <99731713+thiagoqm@users.noreply.github.com>
2022-02-17 16:11:49 -0300

    Update books_spider.py

--- books_spider.py
+++ books_spider.py
@@ -1,20 +1,24 @@
 import scrapy
+import sys
+
+
 
 
 class booksSpider(scrapy.Spider):
     name = "books"
     start_urls =  ['http://books.toscrape.com/']
-    
 
+    
     def parse(self, response):
-        
+
         books = response.xpath('*//li/article[@class="product_pod"]')
-            
-        for a in books:
+        links = response.xpath('*//h3/a/@href').extract()
+                   
+        for a, b in zip(books, links):
             yield{
                 'title':a.xpath('.//h3/a/text()').get(),  #ok
                 'price':a.xpath('.//p[@class="price_color"]/text()').get(), #ok
-                'link':a.xpath('.//h3/a[@href]').get(), #ok
+                'link':f'http://books.toscrape.com/{b}', #ok
                 'img_link':a.xpath('.//div[@class="image_container"]/a[@href]/img[@class="thumbnail"]').get() #ok
                 }
         
